b749612cb1d48b34d7f0c50c2ff196a40cc1de62
root <spacetiller@163.com>
2018-08-06 11:59:59 +0800

    modify to add type

--- go/base/json-2.go
+++ go/base/json-2.go
@@ -7,8 +7,8 @@ import (
 // Product 商品信息
 type Product struct {
     Name      string `json:"name"`
-    ProductID int64 `json:"product_id,omitempty"`  // no blank before omitempty
-    Number    int `json:"number"`
+    ProductID int64 `json:"product_id,omitempty,string"`  // no blank before omitempty
+    Number    int `json:"number,string"`
     Price     float64 `json:"price"`
     IsOnSale  bool `json:"is_on_sale"`
 }
@@ -19,7 +19,7 @@ func main() {
     p.IsOnSale = true
     p.Number = 10000
     p.Price = 2499.00
-    p.ProductID = 0
+    p.ProductID = 10
     data, _ := json.Marshal(p)
     fmt.Println("String(json) result:")
     fmt.Println(string(data))
