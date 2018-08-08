7c81c374521887c19b2a2039247b99cd0979bdd3
root <spacetiller@163.com>
2018-08-08 18:15:17 +0800

    modify string 3

--- go/base/string-3.go
+++ go/base/string-3.go
@@ -21,17 +21,17 @@ func main() {
     fmt.Println(strings.Replace("ababababababab", "ab", "sqn", 5)) //在s字符串中，把old字符串替换为new字符串，n表示替换的次数，小于0表示全部替换
     fmt.Println(strings.Replace("ababababababab", "ab", "sqn", 35))
     fmt.Println(strings.Split("sqn-sqn-sqn.sq-.sqn", "-")) //把s字符串按照sep分割，返回slice
-    fmt.Println(strings.Trim("sqnfewfewsqn", "sqn")) //在s字符串的头部和尾部去除cutset指定的字符串
-    fmt.Println(strings.Join(strings.Fields("a b sqn hello world sqn"), "***")) //去除s字符串的空格符，并且按照空格分割返回slice,并连接
+    fmt.Println(strings.Trim("sqnssfewfewsqn", "sqn")) //在s字符串的头部和尾部去除cutset指定的字符串
+    fmt.Println(strings.Join(strings.Fields("a b sqn hello world sqn"), "--")) //去除s字符串的空格符，并且按照空格分割返回slice,并连接
     /*字符串转化的函数在strconv中*/
     //Append 系列函数将整数等转换为字符串后，添加到现有的字节数组中。
     str := make([]byte, 0, 100)
-    str = strconv.AppendInt(str, 4444, 10) //10进制
-    str = strconv.AppendInt(str, 55555, 2) //2进制
+    str = strconv.AppendInt(str, 44, 10) //10进制
+    str = strconv.AppendInt(str, 5, 2) //2进制
     str = strconv.AppendBool(str, false)
-    str = strconv.AppendQuote(str, "abcdefg")
-    str = strconv.AppendQuoteRune(str, '单')
+    str = strconv.AppendQuote(str, "abcd")
     str = strconv.AppendQuoteRune(str, '你')
+    str = strconv.AppendQuoteRuneToASCII(str, '好')
     fmt.Println(string(str))
 
     //Format 系列函数把其他类型的转换为字符串Go
@@ -54,6 +54,7 @@ func main() {
     e1, err4 := strconv.Atoi("1023")
     checkError(err4)
     fmt.Println(a1, b1, c1, d1, e1)
+
     b3 := "1000"
     fmt.Println(string(b3))
     m := 6
