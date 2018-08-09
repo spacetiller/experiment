804d754e6819f1d5d3e8bdd8ddb93d890c154163
root <spacetiller@163.com>
2018-08-09 19:09:53 +0800

    add lcd

---
+++ go/algo/lcd.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+//	"math"
+)
+
+func gcd(a int, b int) int{
+	if b == 0 {
+		return a
+	}else{
+		//g := gcd(b, int(math.Mod(a,b)))
+		r := a%b
+		fmt.Println(r)
+		g := gcd(b, r)
+		return g
+	}
+}
+
+func main() {
+	//var q [100]int /* n 是一个长度为 10 的数组 */
+	//var r [100]int /* n 是一个长度为 10 的数组 */
+	//var i, j int
+	
+	var a,n int = 10,15
+	fmt.Printf("GCD of %d and %d: %d\n", a, n, gcd(a,n))
+
+}
