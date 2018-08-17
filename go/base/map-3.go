ed9acc62194d701e4e9377f37837f743c1580be5
root <spacetiller@163.com>
2018-08-17 19:46:39 +0800

    add map delete

--- go/base/map-3.go
+++ go/base/map-3.go
@@ -15,8 +15,9 @@ func main() {
 	// 往这个map里插入几条数据
 	personDB["12345"] = PersonInfo{"12345", "Tom", "Room 203,..."}
 	personDB["1"] = PersonInfo{"1", "Jack", "Room 101,..."}
+	delete(personDB, "1")
 	// 从这个map查找键为 "1234" 的信息
-	person, ok := personDB["1234"] // ok是一个返回的bool型，返回true表示找到了对应的数据
+	person, ok := personDB["12345"] // ok是一个返回的bool型，返回true表示找到了对应的数据
 	if ok {
 		fmt.Println("Found person ", person.Name, " with ID 1234.")
 	} else {
