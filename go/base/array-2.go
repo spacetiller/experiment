2ef35cdb837a59196fa2766bf64eadec514fcdf9
root <spacetiller@163.com>
2018-08-17 18:30:22 +0800

    len and cap

--- go/base/array-2.go
+++ go/base/array-2.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var n [10]int /* n 是一个长度为 10 的数组 */
+	//var n [10]int /* n 是一个长度为 10 的数组 */
+	n := make([]int, 5, 10)
 	var i, j int
 
 	/* 为数组 n 初始化元素 */
-	for i = 0; i < 10; i++ {
+	for i = 0; i < 5; i++ {
 		n[i] = i + 100 /* 设置元素为 i + 100 */
 	}
 
@@ -15,6 +16,8 @@ func main() {
 	for j = 0; j < 10; j++ {
 		fmt.Printf("Element[%d] = %d\n", j, n[j])
 	}
+	fmt.Println("len(n):", len(n))
+	fmt.Println("cap(n):", cap(n))
 	//println(n) 	// 错误：illegal types for operand: print	[10]int
 	println(&n[0])
 }
