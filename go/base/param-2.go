60cfb1ad6f4b5b5afc5fce3aed5380b779a0403f
root <spacetiller@163.com>
2018-08-24 15:15:22 +0800

    add webserver

--- go/base/param-2.go
+++ go/base/param-2.go
@@ -10,9 +10,7 @@ func main() {
     flag.StringVar(&src, "src", "", "source file")
     var level *int
     level = flag.Int("level", 0, "debug level")
-    var memo string
-    flag.StringVar(&memo, "memo", "", "the memory")
     flag.Parse()
     flag.Usage()
-    fmt.Printf("src=%s, level=%d, memo=%s\n", src, *level, memo)
+    fmt.Printf("src=%s, level=%d\n", src, *level)
 }
