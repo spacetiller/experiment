8b2dba852a1b5366c0e13fbd8acd3c7deec2f15b
root <spacetiller@163.com>
2018-09-14 14:22:50 +0800

    defer can read return param

--- go/base/defer-2.go
+++ go/base/defer-2.go
@@ -2,14 +2,15 @@ package main
 
 import "fmt"
 
-func a() {
+func a()(j int) {
 	i := 0
-	defer fmt.Println(i) // print 0 because Rule#1: the parameter of defer is resolved real-time
+	defer fmt.Println(i)  // print 0 because Rule#1: the parameter of defer is resolved real-time
 	i++
-	defer fmt.Println(i) // Rule#2: defer funcs subject to the FILO rule 
-	return
+	defer fmt.Println(i)  // Rule#2: defer funcs subject to the FILO rule 
+	defer func(){ j++ }() // Rule#3: defer can read return params
+	return 5
 }
 
 func main() {
-	a()
+	fmt.Println(a())
 }
