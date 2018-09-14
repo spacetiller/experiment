c4c4834de21ce5580819dcf8473916f85d415f9b
root <spacetiller@163.com>
2018-09-14 14:16:00 +0800

    defer FILO rule

--- go/base/defer-2.go
+++ go/base/defer-2.go
@@ -6,6 +6,7 @@ func a() {
 	i := 0
 	defer fmt.Println(i) // print 0 because Rule#1: the parameter of defer is resolved real-time
 	i++
+	defer fmt.Println(i) // Rule#2: defer funcs subject to the FILO rule 
 	return
 }
 
