6d427c96a59221579846ebf5d924e93df4563f21
spacetiller <spacetiller@163.com>
2018-12-04 09:50:14 +0800

    buffer compare

---
+++ nodejs/buffer-compare.js
@@ -0,0 +1,11 @@
+var buffer1 = Buffer.from('ABC');
+var buffer2 = Buffer.from('ABCD');
+var result = buffer1.compare(buffer2);
+
+if(result < 0) {
+   console.log(buffer1 + " 在 " + buffer2 + "之前");
+}else if(result == 0){
+   console.log(buffer1 + " 与 " + buffer2 + "相同");
+}else {
+   console.log(buffer1 + " 在 " + buffer2 + "之后");
+}
