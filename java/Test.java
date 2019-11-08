db9a3f9ed9fdcc37575704cc8eefa7037b2e3995
张辉 <spacetiller@163.com>
2019-11-08 14:52:39 +0800

    update Test

--- java/Test.java
+++ java/Test.java
@@ -5,6 +5,7 @@
  * @author Juncheng
  * @version 0.1
  */
+import java.math.BigDecimal;
 public class Test
 {
       //程序的主函数入门
@@ -20,10 +21,32 @@ public class Test
 		  float f2 = f1;
 	  
 	      System.out.println("Charzh: " + czh);
-	      System.out.println("Double: " + d);
+	      System.out.println("Double: " + Double.toString(d));
 	      System.out.println("Float : " + f);
-		  System.out.println("Double 的取值范围：" + Double.MAX_VALUE+"---->" + Double.MIN_VALUE);
+		  System.out.printf("1000.0/3.0: %,.2f",1000.0/3.0);
+		  System.out.println("Double 的取值范围：\n" + Double.MAX_VALUE+"---->" + Double.MIN_VALUE);
 		  System.out.println("Float 的取值范围：" + Float.MAX_VALUE+"---->" + Float.MIN_VALUE);
 	      System.out.println("Float 1 : " + f2);
+
+		  BigDecimal bd = new BigDecimal(Double.toString(d));
+		  System.out.println("BigDecimal: " + String.valueOf(bd));
+		  System.out.println("BigDecimal::multiply() : " + bd.multiply(new BigDecimal(0.1)).doubleValue());
+
+		  long creditCardNumber = 1234_5678_9012_3456L;
+long socialSecurityNumber = 999_99_9999L;
+float pi = 	3.14_15F;
+long hexBytes = 0xFF_EC_DE_5E;
+long hexWords = 0xCAFE_BABE;
+long maxLong = 0x7fff_ffff_ffff_ffffL;
+byte nybbles = 0b0010_0101;
+long bytes = 0b11010010_01101001_10010100_10010010;
+		
+		System.out.println(new BigDecimal(0.1).toString()); // 0.1000000000000000055511151231257827021181583404541015625
+System.out.println(new BigDecimal("0.1").toString()); // 0.1
+System.out.println(new BigDecimal(
+Double.toString(0.1000000000000000055511151231257827021181583404541015625)).toString());// 0.1
+System.out.println(new BigDecimal(Double.toString(0.1)).toString()); // 0.1
+
+
 	  }
 }
---
+++ java/Test5.java
@@ -0,0 +1,62 @@
+public class Test5 {
+
+private static int a;
+
+private int b;
+
+ 
+
+static {
+
+Test5.a = 3;
+
+System.out.println(a);
+
+Test5 t = new Test5();
+
+t.f();
+
+t.b = 1000;
+
+System.out.println(t.b);
+System.out.println(t.a);
+
+}
+
+ 
+
+static {
+
+Test5.a = 4;
+
+System.out.println(a);
+
+}
+
+ 
+
+public static void main(String[] args) {
+
+// TODO 自动生成方法存根
+
+}
+
+ 
+
+static {
+
+Test5.a = 5;
+
+System.out.println(a);
+
+}
+
+ 
+
+public void f() {
+
+System.out.println("hhahhahah");
+
+}
+
+}
