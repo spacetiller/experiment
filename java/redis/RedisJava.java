2dada623b6d48fbbc076682f962c0410b200fffa
zhanghui <zhanghui@wecash.net>
2020-12-18 14:43:41 +0800

    add jredis

--- java/redis/RedisJava.java
+++ java/redis/RedisJava.java
@@ -3,7 +3,8 @@ import redis.clients.jedis.Jedis;
 public class RedisJava {
     public static void main(String[] args) {
         // 连接本地的 Redis 服务
-        Jedis jedis = new Jedis("localhost");
+        Jedis jedis = new Jedis("");
+		//jedis.auth("");
         System.out.println("连接本地的 Redis 服务成功！");
         // 查看服务是否运行
         System.out.println("服务 正在运行: " + jedis.ping());
